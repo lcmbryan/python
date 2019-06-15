775dfec488797e5f92825586b2028730486622b8
lcmbryan <49527866+lcmbryan@users.noreply.github.com>
2019-06-15 10:27:47 +0800

    Create qa_utility.py

---
+++ qa_utility.py
@@ -0,0 +1,49 @@
+########################################################
+##  Author      : Bryan Leong
+##  Date        : 5 June 2019
+##  Version     : 0.1
+##  Description : Draft version of qa utility menu
+########################################################
+
+import os
+import platform
+
+def clear_screen():
+
+    if platform.system()=='Windows':
+        os.system("cls")
+    if platform.system()=='Linux':
+        os.system("clear")
+    
+def menu():
+
+    while True:
+        print("--------------------")
+        print("QA Utility")
+        print("--------------------")
+        print("1. Installation")
+        print("2. Smoke Test")
+        print("3. Regression Test")
+        print("4. Quit")
+        choice = int(input("Enter your choice: "))
+        clear_screen()
+        
+        if choice == 1:
+            print("You choose to install new SAA")
+            
+        elif choice == 2:
+            print("You choose to run smoke test")
+            
+        elif choice == 3:
+            print("You choose to regression test")
+            
+        elif choice == 4:
+            print("Bye")
+            break
+            exit
+        else:
+            print("Invalid choice. Enter 1-3")
+            menu()
+
+## Main routine
+menu()
